19a2dcc2ae9518baba1cbcba8b5d3969bffb7e83
jaquepaschoal <jaque-paschoal@hotmail.com>
2018-05-03 16:01:09 -0300

    create page 404.html

---
+++ development/src/js/error.js
@@ -0,0 +1,7 @@
+(function(document){
+  'use strict'
+
+  var $text = document.querySelector('[data-js="text"]');
+  
+  console.log($text);
+})(document)
--- development/src/js/scripts.js
+++ development/src/js/scripts.js
@@ -5,21 +5,49 @@
     var $areaIcon   = document.querySelector('[data-js="area-icon"]');
     var $icon       = document.querySelector('[data-js="icon"]');
     var $menuMobile = document.querySelector('[data-js="menu-mobile"]');
-    
-    $areaIcon.addEventListener('click',showMenu);
 
-    function showMenu(){
-      $menuMobile.classList.toggle('display-none');
-      $icon.classList.contains('fa-bars') ? $icon.className = 'far fa-times' : $icon.className = 'far fa-bars';
+    if($areaIcon) {
+      $areaIcon.addEventListener('click',showMenu);
+
+      function showMenu(){
+        $menuMobile.classList.toggle('display-none');
+        $icon.classList.contains('fa-bars') ? $icon.className = 'far fa-times' : $icon.className = 'far fa-bars';
+      }
     }
+
+    var $text = document.querySelector('[data-js="text"]');
+    if($text) {
+      function typeWriter(element) {
+        var textArray = element.innerHTML.split('');
+        element.innerHTML='';
+        
+        textArray.forEach(function(letter,i){
+
+          setTimeout(function(){
+            element.innerHTML += letter;
+          },75 * i)
+        })
+        
+      }
+
+      typeWriter($text);
+    }
+  
+   
 })(document);
 
-$('.nav a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
-});
+(function(){
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+        
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
+})()
+
+
+
+
